be65c20179b045118a8ba7be6453061a0a3aa9a5
Jean-Noël Vouilloz <jean-noel.vouilloz@bestmile.com>
2018-06-15 15:08:23 +0200

    get what to update from git pull

--- helmAPI.go
+++ helmAPI.go
@@ -6,41 +6,53 @@ import (
     "net/http"
     "os/exec"
     "bytes"
-    "strings"
+    "regexp"
     "github.com/gorilla/mux"
 )
+
 func shell(w http.ResponseWriter, r *http.Request) {
-vars := mux.Vars(r)
-var out bytes.Buffer
-workDir := "/Users/ichtar/git/helm-charts"
-// force endpoint to accept only specific values send a forbidden otherwise
-if strings.EqualFold(vars["cluster"],"eu1.bestmile.com") && strings.EqualFold(vars["deployment"],"test") {
+    var out bytes.Buffer
+    var envPath,cluster,deployment string
+    workDir := "/Users/ichtar/git/helm-charts"
     // get last version of code
     cmd := exec.Command("git","pull","--rebase")
     cmd.Dir = workDir
+    cmd.Stdout = &out
     err := cmd.Run()
     if err != nil {
         log.Fatal(err)
     }
-    envPath := strings.Join([]string{"environments/",vars["cluster"],"/",vars["deployment"],".yaml"},"")
-    // remove dry-run when want to push real update
-    cmd = exec.Command("helm","--dry-run","--debug","--kube-context",vars["cluster"],"upgrade",vars["deployment"],"-f",envPath,"charts/bm-stack")
-    cmd.Dir = workDir
-    cmd.Stdout = &out
-    err = cmd.Run()
-    if err != nil {
-	log.Fatal(err)
-    }
+    // get all informations from pulled file
+    myR := regexp.MustCompile(".*(environments/([^/]*)/(.*).yaml).*")
+    searchResult := myR.FindAllStringSubmatch(out.String(),-1)
+    // if it match nothing in format environments/{cluster}/{deployment}.yaml abort and send Notfound
+    if searchResult != nil {
+        for _,i := range searchResult {
+            envPath=i[1]
+            cluster=i[2]
+            deployment=i[3]
+        }
+        // remove dry-run when want to push real update
+        cmd = exec.Command("helm","--dry-run","--debug","--kube-context",cluster,"upgrade",deployment,"-f",envPath,"charts/bm-stack")
+        cmd.Dir = workDir
+        cmd.Stdout = &out
+        err = cmd.Run()
+        if err != nil {
+            log.Fatal(err)
+        }
         fmt.Fprintf(w, out.String())
-    } else {
+      } else {
         http.Error(w, "Forbidden", http.StatusForbidden)
     }
 }
+
 func handleRequests() {
     myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/trigger/{cluster}/{deployment}", shell ).Methods("PUT")
+    myRouter.HandleFunc("/trigger", shell ).Methods("POST")
     log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
+
 func main() {
     handleRequests()
 }
+
